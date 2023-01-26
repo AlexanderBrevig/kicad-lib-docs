26066ca55154b1b968577ba2893eac8f7c0117be
Alexander Brevig <alexanderbrevig@gmail.com>
2023-01-26 09:57:36 +0100

    test: setup test for docgen.rs

--- src/docgen.rs
+++ src/docgen.rs
@@ -19,3 +19,29 @@ pub fn find(val: &Value, root: &str, qualifier: &str, offset: usize) -> String {
     }
     return "".to_string();
 }
+
+#[cfg(test)]
+mod tests {
+    use lexpr::parse::Error;
+
+    const S_EXPRESSION: &'static str = r#"((dummy . "nothing")
+        (name "first" "John")
+        (age . 43)
+        (phones "+44 1234567" "+44 2345678"))"#;
+
+    #[test]
+    fn find_can_traverse() -> Result<(), Error> {
+        let v = lexpr::from_str(S_EXPRESSION)?;
+        let name = crate::docgen::find(&v, "name", "first", 2);
+        assert_eq!(name, "\"John\"");
+        Ok(())
+    }
+
+    #[test]
+    fn find_fail_traverse_with_blank() -> Result<(), Error> {
+        let v = lexpr::from_str(S_EXPRESSION)?;
+        let name = crate::docgen::find(&v, "nothing", "first", 2);
+        assert_eq!(name, "");
+        Ok(())
+    }
+}
